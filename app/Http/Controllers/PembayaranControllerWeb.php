7c7cc8655cccbdcb4871e90fc19277e92d8609a0
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-12-08 19:28:44 +0700

    tes

--- app/Http/Controllers/PembayaranControllerWeb.php
+++ app/Http/Controllers/PembayaranControllerWeb.php
@@ -16,24 +16,24 @@ class PembayaranControllerWeb extends Controller
     public function dashboard()
     {
         // Ambil data pembayaran dengan relasi ke KajianModel
-        $pembayaran = PembayaranModel::with('kajian')->get();
+        // $pembayaran = PembayaranModel::with('kajian')->get();
 
-        // Ambil data harga dari KajianModel
-        $salesData = $pembayaran->map(function ($pembayaran) {
-            return [
-                'date' => $pembayaran->created_at->format('F Y'), // Ambil bulan dan tahun dari pembayaran
-                'price' => $pembayaran->kajian->price, // Ambil harga dari KajianModel
-            ];
-        });
+        // // Ambil data harga dari KajianModel
+        // $salesData = $pembayaran->map(function ($pembayaran) {
+        //     return [
+        //         'date' => $pembayaran->created_at->format('F Y'), // Ambil bulan dan tahun dari pembayaran
+        //         'price' => $pembayaran->kajian->price, // Ambil harga dari KajianModel
+        //     ];
+        // });
 
-        // Ubah ke koleksi dan Kelompokkan data berdasarkan bulan
-        $groupedSales = collect($salesData)->groupBy('date')->map(function ($monthSales) {
-            return $monthSales->sum('price'); // Menghitung total harga per bulan
-        });
+        // // Ubah ke koleksi dan Kelompokkan data berdasarkan bulan
+        // $groupedSales = collect($salesData)->groupBy('date')->map(function ($monthSales) {
+        //     return $monthSales->sum('price'); // Menghitung total harga per bulan
+        // });
 
-        $total = PembayaranModel::sum('jumlah');
+        // $total = PembayaranModel::sum('jumlah');
 
         // Kirim data ke view
-        return view('welcome', compact('groupedSales'));
+        return view('welcome');
     }
 }
