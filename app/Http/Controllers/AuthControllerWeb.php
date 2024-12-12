0d14204ec7ac3dd6be4a3c1f6a8b638c6c553f25
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-12-12 17:56:13 +0700

    fix

--- app/Http/Controllers/AuthControllerWeb.php
+++ app/Http/Controllers/AuthControllerWeb.php
@@ -15,6 +15,8 @@ class AuthControllerWeb extends Controller
 
     public function authenticate(Request $request)
     {
+
+        // Validasi inputan
         $credentials = $request->validate([
             'email' => ['required'],
             'password' => ['required'],
@@ -24,21 +26,54 @@ class AuthControllerWeb extends Controller
             $request->session()->regenerate();
 
             // Memeriksa peran pengguna setelah login
-            if (Auth::user()->role === 1) {
-                // Jika peran pengguna adalah 1, biarkan mereka masuk
-                return redirect()->intended('/user');
-            } elseif (Auth::user()->role === 2) {
-                // Jika peran pengguna adalah 2, log out dan tampilkan pesan kesalahan
+            $user = Auth::user();
+            // dd($user->role);
+
+            if ($user->role === 2) {
+                // Logout jika role adalah 2
                 Auth::logout();
+                session()->flush();
                 return back()->with('loginError', 'Akses tidak diizinkan untuk pengguna dengan peran ini.');
-            } else {
-                // Untuk peran lainnya, Anda bisa menambahkan penanganan lebih lanjut jika diperlukan
-                return redirect()->intended('/login');
             }
+
+            // Redirect berdasarkan role
+            if ($user->role === 1) {
+                return redirect()->intended('/user');
+            }
+
+            return redirect()->intended('/login');
         }
 
         return back()->with('loginError', 'Login gagal! Silakan periksa email dan password.');
     }
+
+
+    // public function authenticate(Request $request)
+    // {
+    //     $credentials = $request->validate([
+    //         'email' => ['required'],
+    //         'password' => ['required'],
+    //     ]);
+
+    //     if (Auth::attempt($credentials)) {
+    //         $request->session()->regenerate();
+
+    //         // Memeriksa peran pengguna setelah login
+    //         if (Auth::user()->role === 1) {
+    //             // Jika peran pengguna adalah 1, biarkan mereka masuk
+    //             return redirect()->intended('/user');
+    //         } elseif (Auth::user()->role === 2) {
+    //             // Jika peran pengguna adalah 2, log out dan tampilkan pesan kesalahan
+    //             Auth::logout();
+    //             return back()->with('loginError', 'Akses tidak diizinkan untuk pengguna dengan peran ini.');
+    //         } else {
+    //             // Untuk peran lainnya, Anda bisa menambahkan penanganan lebih lanjut jika diperlukan
+    //             return redirect()->intended('/login');
+    //         }
+    //     }
+
+    //     return back()->with('loginError', 'Login gagal! Silakan periksa email dan password.');
+    // }
 
 
     public function logout(Request $request)
